7866c3119bbcb00ad9fe4aeb95769b4191e37c02
Franz <franz.hanschmidt@posteo.de>
2021-06-07 16:19:57 +0200

    add level2

--- src/App.js
+++ src/App.js
@@ -4,19 +4,25 @@ import "./App.css";
 
 function App() {
   const [memes, setMemes] = useState();
-  const [captions, setCaptions] = useState();
+  const [captions, setCaptions] = useState({
+    upperCaption: "the upper caption goes here",
+    lowerCaption: "your lower caption goes here",
+  });
   const [activeMeme, setActiveMeme] = useState();
+  const [userUpload, setUserUpload] = useState();
 
   useEffect(() => {
     const fetchPictures = async () => {
       try {
         const {
-          data: { data: dataImgflip },
+          data: {
+            data: { memes: dataImgflip },
+          },
         } = await axios.get("https://api.imgflip.com/get_memes");
-        console.log(dataImgflip);
+
+        // do sth with dataImgflip
         setMemes(dataImgflip);
-        setActiveMeme(dataImgflip.memes[0]);
-        console.log("s");
+        setActiveMeme(dataImgflip[0]);
       } catch (e) {
         console.log(e.message);
       }
@@ -24,42 +30,80 @@ function App() {
     fetchPictures();
   }, []);
 
-  const onChangePicture = () => {
-    setActiveMeme(memes[2]);
+  // 1st option => map over each meme and add a isActive property ==> complicated
+  // 2nd option => have an activeMeme state and put a random meme inside ==> simple
+
+  const onChangePicture = (e) => {
+    e.preventDefault();
+    console.log(memes[3]);
+    setActiveMeme(memes[Math.floor(Math.random() * 100)]);
   };
   const handleChange = (e) => {
-    console.log(e.target.form.upperCaption.value);
-    setCaptions({
-      upper: e.target.form.upperCaption.value,
-      lower: e.target.form.upperCaption.value,
-    });
+    // console.log(e.target.name);
+    //console.log(e.target.form.upperCaption.value);
+    setCaptions({ ...captions, [e.target.name]: e.target.value });
   };
+
+  const handleUploadFile = (e) => {
+    if (activeMeme) {
+      URL.revokeObjectURL(userUpload.url);
+    }
+    console.log(e.target.files);
+    const userURL = URL.createObjectURL(e.target.files[0]);
+    console.log(userURL);
+    setActiveMeme({ url: userURL });
+    //URL.revokeObjectURL(userURL);
+  };
+
+  const handleUploadClick = (e) => {
+    e.preventDefault();
+    //console.log(e.target.form.fileElem);
+    const fileElem = e.target.form.fileElem;
+    fileElem.click();
+  };
+
   return (
     <div>
       <header>
         <h1>I can has memes!</h1>
       </header>
       <main>
-        <form onChange={handleChange}>
-          <fieldset>
-            <input placeholder="Upper caption" name="upperCaption"></input>
-            <input placeholder="Lower caption" name="lowerCaption"></input>
+        <form>
+          <fieldset onChange={handleChange}>
+            <input
+              placeholder="Upper caption"
+              name="upperCaption"
+              value={captions.upperCaption}
+            ></input>
+            <input
+              placeholder="Lower caption"
+              name="lowerCaption"
+              value={captions.lowerCaption}
+            ></input>
           </fieldset>
           <fieldset>
             <button name="changePicture" onClick={onChangePicture}>
               Change picture
             </button>
-            <button name="loadPicture">Load picture</button>
-            <button name="generateMeme" type="submit">
-              Generate Meme
+            <input
+              onChange={handleUploadFile}
+              type="file"
+              name="fileElem"
+              multiple
+              accept="image/*"
+              style={{ display: "none" }}
+            />
+            <button name="loadPicture" onClick={handleUploadClick}>
+              Load picture
             </button>
+            <button name="generateMeme">Generate Meme</button>
           </fieldset>
         </form>
         {activeMeme && (
           <section className="captionSection">
             <img src={activeMeme.url}></img>
-            <div className="upperCaption">Upper caption</div>
-            <div className="lowerCaption">Lower caption</div>
+            <div className="upperCaption">{captions.upperCaption}</div>
+            <div className="lowerCaption">{captions.lowerCaption}</div>
           </section>
         )}
       </main>

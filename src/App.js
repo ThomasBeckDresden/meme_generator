029799fff11c5a11b57e2394d113392881ba167c
Franz <franz.hanschmidt@posteo.de>
2021-06-07 11:24:54 +0200

    add html structure and fetch

--- src/App.js
+++ src/App.js
@@ -1,23 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css";
 
 function App() {
+  const [memes, setMemes] = useState();
+  const [captions, setCaptions] = useState();
+  useEffect(() => {
+    const fetchPictures = async () => {
+      try {
+        const {
+          data: { data: dataImgflip },
+        } = await axios.get("https://api.imgflip.com/get_memes");
+        console.log(dataImgflip);
+        setMemes(dataImgflip);
+      } catch (e) {
+        console.log(e.message);
+      }
+    };
+    fetchPictures();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header>
+        <h1>I can has memes!</h1>
       </header>
+      <main>
+        <form>
+          <fieldset>
+            <input placeholder="Upper caption" name="upperCaption"></input>
+            <input placeholder="Lower caption" name="lowerCaption"></input>
+          </fieldset>
+          <fieldset>
+            <button name="changePicture">Change picture</button>
+            <button name="loadPicture">Load picture</button>
+            <button name="generateMeme" type="submit">
+              Generate Meme
+            </button>
+          </fieldset>
+        </form>
+        {memes && (
+          <section className="captionSection">
+            <img src={memes.memes[1].url}></img>
+            <div className="upperCaption">Upper caption</div>
+            <div className="lowerCaption">Lower caption</div>
+          </section>
+        )}
+      </main>
     </div>
   );
 }
